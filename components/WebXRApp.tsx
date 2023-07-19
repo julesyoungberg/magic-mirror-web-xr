97922cdefcc1faa44188a11a374d2d59d3ad0254
Jules Youngberg <julesdayoungberg@gmail.com>
2023-07-18 20:11:13 -0700

    initial three.js

---
+++ components/WebXRApp.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+
+import Box from "./Box";
+
+export default function WebXRApp() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+}
