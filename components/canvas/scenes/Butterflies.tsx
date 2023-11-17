63f80d523772380a4e56d8491ebb6c454ae59bb5
Jules Youngberg <julesdayoungberg@gmail.com>
2023-11-16 20:11:10 -0800

    add butterflies todo

---
+++ components/canvas/scenes/Butterflies.tsx
@@ -0,0 +1,11 @@
+/**
+ * @todo butterflies in AR
+ * based on:
+ * - https://ykob.github.io/sketch-threejs/sketch/butterfly.html
+ * - https://github.com/ykob/sketch-threejs/blob/main/src/js/sketch/butterfly/Butterfly.js
+ *
+ * Could use other models: https://www.turbosquid.com/3d-model/free/butterfly
+ *
+ * Could use theatre.js but probably not needed: https://www.theatrejs.com/docs/0.5/concepts
+ *
+ */
