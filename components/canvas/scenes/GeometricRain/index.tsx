725bb426fea495356510eba2d0dfed92d8d7b190
Jules Youngberg <julesdayoungberg@gmail.com>
2023-11-16 19:31:21 -0800

    wip - geometric rain

--- components/canvas/scenes/GeometricRain/index.tsx
+++ components/canvas/scenes/GeometricRain/index.tsx
@@ -1,23 +1,17 @@
 // @todo falling shapes with physics
 import * as THREE from "three";
-import React, { useMemo, useRef } from "react";
-import { useFrame, useThree } from "@react-three/fiber";
+import React, { useMemo, useRef, useState } from "react";
 import { blendshapesMap, useFaceLandmarks } from "@/hooks/useFaceLandmarks";
 import { useWebcam } from "@/hooks/useWebcam";
-import {
-    MeshCollider,
-    InstancedRigidBodies,
-    Physics,
-    RapierRigidBody,
-    RigidBody,
-} from "@react-three/rapier";
+import { Physics } from "@react-three/rapier";
 import niceColors from "nice-color-palettes";
 import { Background } from "../../Background";
 import { HeadModel } from "./HeadModel";
+import { InstanceGroup } from "./InstanceGroup";
 
 // number of shapes to create each frame
 const N_SHAPES = 100;
-const MAX_SHAPES = 100;
+const MAX_SHAPES = 200;
 
 const tempColor = new THREE.Color();
 const data = Array.from({ length: MAX_SHAPES }, () => ({
@@ -25,22 +19,9 @@ const data = Array.from({ length: MAX_SHAPES }, () => ({
     scale: 1,
 }));
 
-const initialShapes = new Array(MAX_SHAPES).fill(0).map((_, i) => ({
-    key: `shape_${i}`,
-    live: false,
-    position: [Math.random() * 10 - 5, 5, Math.random() * 4 - 2],
-    scale: [0.5, 0.5, 0.5],
-    rotation: [1, 0, 0], // Math.random(), Math.random(), Math.random()],
-}));
-
-// console.log(initialShapes);
-
 export function GeometricRain() {
     const webcam = useWebcam();
-    const { camera } = useThree();
-    const faceDetected = useRef(false);
-    const instancedMeshRef = useRef<THREE.InstancedMesh>(null!);
-    const rigidBodiesRef = useRef<RapierRigidBody[]>(null);
+    const [faceDetected, setFaceDetected] = useState(false);
     const colorArray = useMemo(
         () =>
             Float32Array.from(
@@ -91,83 +72,28 @@ export function GeometricRain() {
                     }
                 }
             }
+
+            setFaceDetected(true);
+        } else {
+            setFaceDetected(false);
         }
 
         if (headMeshRef.current) {
             headMeshRef.current.needsUpdate = true;
         }
-
-        faceDetected.current = true;
     });
 
-    useFrame(() => {
-        let resetNShapes = 0;
-        if (faceDetected.current) {
-            resetNShapes = N_SHAPES;
-        }
-
-        for (let i = 0; i < MAX_SHAPES; i++) {
-            const rigidBody = rigidBodiesRef.current?.[i];
-            if (!rigidBody) {
-                continue;
-            }
-
-            const position = rigidBody.translation();
-
-            const frustum = new THREE.Frustum();
-            const matrix = new THREE.Matrix4().multiplyMatrices(
-                camera.projectionMatrix,
-                camera.matrixWorldInverse
-            );
-            frustum.setFromProjectionMatrix(matrix);
-
-            if (
-                !frustum.containsPoint(
-                    new THREE.Vector3(position.x, position.y, position.z)
-                )
-            ) {
-                if (resetNShapes > 0) {
-                    // resset shape
-                    rigidBody.setTranslation(
-                        {
-                            x: Math.random() * 10 - 5,
-                            y: 10,
-                            z: Math.random() * 4 - 2,
-                        },
-                        true
-                    );
-                    rigidBody.setAngvel({ x: 0, y: 0, z: 0 }, true);
-                    rigidBody.setLinvel({ x: 0, y: 0, z: 0 }, true);
-                    resetNShapes--;
-                    if (resetNShapes == 0) {
-                        break;
-                    }
-                } else {
-                    rigidBody.sleep();
-                }
-            }
-        }
-    });
-
-    console.log({ headGroupRef, headMeshRef });
+    console.log({ headGroupRef, headMeshRef, faceDetected });
 
     return (
-        <Physics gravity={[0, -1, 0]} interpolate={false} colliders={false}>
+        <Physics gravity={[0, -9.8, 0]} interpolate={false} colliders={false}>
             {webcam && <Background texture={webcam.texture} />}
-            {/*<RigidBody position={[0, 10, 0]} colliders="ball">
-                <MeshCollider type="hull">
-                    <HeadModel groupRef={headGroupRef} meshRef={headMeshRef} />
-                </MeshCollider>
-            </RigidBody>*/}
             <HeadModel groupRef={headGroupRef} meshRef={headMeshRef} />
-            <InstancedRigidBodies
-                ref={rigidBodiesRef}
-                instances={initialShapes}
-            >
-                <instancedMesh
-                    ref={instancedMeshRef}
-                    args={[null as any, null as any, MAX_SHAPES]}
-                    count={MAX_SHAPES}
+            {faceDetected.current && (
+                <InstanceGroup
+                    faceDetected={faceDetected.current}
+                    maxShapes={MAX_SHAPES}
+                    newShapesRate={N_SHAPES}
                 >
                     <boxGeometry>
                         <instancedBufferAttribute
@@ -176,8 +102,8 @@ export function GeometricRain() {
                         />
                     </boxGeometry>
                     <meshBasicMaterial toneMapped={false} vertexColors />
-                </instancedMesh>
-            </InstancedRigidBodies>
+                </InstanceGroup>
+            )}
             <pointLight castShadow />
         </Physics>
     );
--- components/canvas/scenes/GeometricRain/instanceGroup.tsx
+++ components/canvas/scenes/GeometricRain/instanceGroup.tsx
@@ -1,16 +1,107 @@
-// @todo falling shapes with physics
 import * as THREE from "three";
-import React, { useEffect, useMemo, useRef } from "react";
-import { useGLTF } from "@/hooks/useGLTF";
-import { useThree } from "@react-three/fiber";
-import { blendshapesMap, useFaceLandmarks } from "@/hooks/useFaceLandmarks";
-import { useWebcam } from "@/hooks/useWebcam";
-import niceColors from "nice-color-palettes";
-import { Background } from "../../Background";
+import React, { ReactNode, useMemo, useRef } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import { InstancedRigidBodies, RapierRigidBody } from "@react-three/rapier";
 
 // number of shapes to create each frame
-const N_SHAPES = 1;
+function newPosition() {
+    return [
+        Math.random() * 10 - 5,
+        5 + Math.random() * 2.0,
+        Math.random() * 6 - 3,
+    ];
+}
 
-const tempObject = new THREE.Object3D();
-const tempColor = new THREE.Color();
-const colorArray = niceColors[17].map((c) => tempColor.set(c).toArray());
+type Props = {
+    children: ReactNode;
+    faceDetected: boolean;
+    maxShapes: number;
+    newShapesRate: number;
+};
+
+// currently works on hot reload but not on initial load of the page
+export function InstanceGroup({
+    children,
+    faceDetected,
+    maxShapes,
+    newShapesRate,
+}: Props) {
+    const { camera } = useThree();
+    const instancedMeshRef = useRef<THREE.InstancedMesh>(null!);
+    const rigidBodiesRef = useRef<RapierRigidBody[]>(null);
+
+    const initialShapes = useMemo(
+        () =>
+            new Array(maxShapes).fill(0).map((_, i) => ({
+                key: `shape_${i}`,
+                live: false,
+                position: [Math.random() * 10 - 5, -5, Math.random() * 6 - 3],
+                scale: [0.5, 0.5, 0.5],
+                rotation: [1, 0, 0], // Math.random(), Math.random(), Math.random()],
+            })),
+        []
+    );
+
+    console.log("InstanceGroup");
+
+    useFrame(() => {
+        console.log({ faceDetected });
+        let resetNShapes = 0;
+        if (faceDetected) {
+            resetNShapes = newShapesRate;
+        }
+
+        for (let i = 0; i < maxShapes; i++) {
+            const rigidBody = rigidBodiesRef.current?.[i];
+            if (!rigidBody) {
+                continue;
+            }
+
+            const position = rigidBody.translation();
+
+            const frustum = new THREE.Frustum();
+            const matrix = new THREE.Matrix4().multiplyMatrices(
+                camera.projectionMatrix,
+                camera.matrixWorldInverse
+            );
+            frustum.setFromProjectionMatrix(matrix);
+
+            if (
+                !frustum.containsPoint(
+                    new THREE.Vector3(position.x, position.y, position.z)
+                ) &&
+                position.y < 0.0
+            ) {
+                if (resetNShapes > 0) {
+                    // resset shape
+                    const [x, y, z] = newPosition();
+                    rigidBody.setTranslation({ x, y, z }, true);
+                    rigidBody.setAngvel({ x: 0, y: 0, z: 0 }, true);
+                    rigidBody.setLinvel({ x: 0, y: 0, z: 0 }, true);
+                    resetNShapes--;
+                    if (resetNShapes == 0) {
+                        break;
+                    }
+                } else if (position.y < -10) {
+                    rigidBody.sleep();
+                }
+            }
+        }
+    });
+
+    return (
+        <InstancedRigidBodies
+            ref={rigidBodiesRef}
+            instances={initialShapes}
+            restitution={0.5}
+        >
+            <instancedMesh
+                ref={instancedMeshRef}
+                args={[null as any, null as any, maxShapes]}
+                count={maxShapes}
+            >
+                {children}
+            </instancedMesh>
+        </InstancedRigidBodies>
+    );
+}
